cb4b40044e4410ed6a7b09b9282cd8560de1523e
Vlad Perfilev <vladislav.perfilev@thomsonreuters.com>
2021-04-24 13:46:11 +0300

    Refactor outcome report

--- src/components/IncomeReport.tsx
+++ src/components/IncomeReport.tsx
@@ -3,85 +3,101 @@ import { State } from "../redux/reducer";
 import { connect } from "react-redux";
 import { AnyAction, bindActionCreators, Dispatch } from "redux";
 
-import { SetSavingsAmount, AddReportRow, DeleteSelectedReportRow, SelectReportRow, RenameReportName } from "./../redux/actionCreators"
+import {
+  SetSavingsAmount,
+  AddReportRow,
+  DeleteSelectedReportRow,
+  SelectReportRow,
+  RenameReportName,
+} from "./../redux/actionCreators";
 import { List, PrimaryButton, SecondaryButton } from "./elements";
 import { AmountRow } from "./";
-import { getSelectedAccountsTransactions } from "../utils/datalogic";
+import {
+  genId,
+  getSelectedAccountsTransactions as getAccTransactions,
+  sumTransactionsByReports,
+} from "../utils/datalogic";
 import { ReportType } from "../models";
 
-const mapStateToProps = (state: State) => ({ 
-    savings: state.savings,
-    reportRows: state.incomeReport,
-    transactions: getSelectedAccountsTransactions(state.transactions, state.accounts, ReportType.income),
-    selectedReportRow: state.selectedReportRow,
-    selectedReportType: state.selectedReportType
+const mapStateToProps = (s: State) => ({
+  savings: s.savings,
+  reportRows: s.incomeReport,
+  transactions: getAccTransactions(s.transactions, s.accounts, ReportType.income),
+  selectedReportRow: s.selectedReportRow,
+  selectedReportType: s.selectedReportType,
 });
 
-const mapDispatchToProps = (dispatch: Dispatch<AnyAction>) => bindActionCreators({ SetSavingsAmount, RenameReportName, AddReportRow, DeleteSelectedReportRow, SelectReportRow }, dispatch );
-type Props = ReturnType<typeof mapStateToProps> & ReturnType<typeof mapDispatchToProps>;
+const mapDispatchToProps = (dispatch: Dispatch<AnyAction>) =>
+  bindActionCreators(
+    {
+      SetSavingsAmount,
+      RenameReportName,
+      AddReportRow,
+      DeleteSelectedReportRow,
+      SelectReportRow,
+    },
+    dispatch
+  );
 
-const IncomeReport: FC<Props> = ({savings, SetSavingsAmount, AddReportRow, DeleteSelectedReportRow, SelectReportRow, 
-    selectedReportType, selectedReportRow, reportRows, transactions, RenameReportName}) =>{
-    const addReport = () => {   
-        AddReportRow({id: (new Date().getTime()).toString(), name: "Расход"}, ReportType.income)
-    }
+type Props = ReturnType<typeof mapStateToProps> &
+  ReturnType<typeof mapDispatchToProps>;
 
-    const removeReport = () => {
-        DeleteSelectedReportRow();
-    }
+const IncomeReport: FC<Props> = ({
+  savings,
+  SetSavingsAmount,
+  AddReportRow,
+  DeleteSelectedReportRow,
+  SelectReportRow,
+  selectedReportType,
+  selectedReportRow,
+  reportRows,
+  transactions,
+  RenameReportName,
+}) => {
+  const reportAmounts = sumTransactionsByReports(transactions);
+  const isRestSelected = selectedReportType === ReportType.income && selectedReportRow === undefined;
 
-    const reportRowSelected = (id: string | undefined) => {
-        SelectReportRow(id);
-    }
-
-    const TitleChanged = (id: string, title: string) => {
-        RenameReportName(id, title);
-    }
-
-    let unreported = 0;
-    const reportAmounts = transactions.reduce<{ [key: string]: number }>((groups, t) => {
-        const k = t.reportId;
-        if (k !== undefined) {
-            let value = groups[k];
-            if (value === undefined) value = 0;
-            value += t.amount;
-            groups[k] = value;
-        } else {
-            unreported += t.amount;
-        }
-        return groups
-    }, {});
-
-    const isRestSelected = selectedReportType === ReportType.income && selectedReportRow === undefined;
-
-    return (
-      <>
-        <List header="Доходы">
-          {reportRows.map((r) => (
-            <AmountRow
-              title={r.name}
-              amount={reportAmounts[r.id] ?? 0}
-              onClick={() => reportRowSelected(r.id)}
-              editType="title"
-              onChanged={(title) => TitleChanged(r.id, title as string)}
-              isSelected={r.id === selectedReportRow?.id}
-            />
-          ))}
-          <AmountRow title="Прочее" amount={unreported} editType="hideIcon" onClick={() => reportRowSelected(undefined)} 
-          isSelected={isRestSelected}/>
+  return (
+    <>
+      <List header="Доходы">
+        {reportRows.map((r) => (
           <AmountRow
-            title="Отложено"
-            amount={savings}
-            editType="amount"
-            onChanged={(value) => SetSavingsAmount(value as number)}
+            title={r.name}
+            amount={reportAmounts[r.id] ?? 0}
+            onClick={() => SelectReportRow(r.id)}
+            editType="title"
+            onChanged={(title) => RenameReportName(r.id, title as string)}
+            isSelected={r.id === selectedReportRow?.id}
           />
-        </List>
-        <div className="flex mt-2">
-          <PrimaryButton text="Добавить" onClick={addReport} />
-          <SecondaryButton text="Удалить" onClick={removeReport} disabled={isRestSelected} />
-        </div>
-      </>
-    );
-}
+        ))}
+        <AmountRow
+          title="Прочее"
+          amount={reportAmounts["unreported"]}
+          editType="hideIcon"
+          onClick={() => SelectReportRow(undefined)}
+          isSelected={isRestSelected}
+        />
+        <AmountRow
+          title="Отложено"
+          amount={savings}
+          editType="amount"
+          onChanged={(value) => SetSavingsAmount(value as number)}
+        />
+      </List>
+      <div className="flex mt-2">
+        <PrimaryButton
+          text="Добавить"
+          onClick={() => AddReportRow({ id: genId(), name: "Расход" }, ReportType.income)
+          }
+        />
+        <SecondaryButton
+          text="Удалить"
+          onClick={DeleteSelectedReportRow}
+          disabled={isRestSelected}
+        />
+      </div>
+    </>
+  );
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(IncomeReport);
